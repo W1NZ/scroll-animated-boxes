041ad73e5b933921fbb2bc7d2e9bfa105408e1f4
Mariusz Czapiewski <m4riusz.cz@gmail.com>
2022-06-17 23:21:59 +0200

    repaired js, litttle changes in css file, added more 'content' class elements to html file

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const content = document.querySelectorAll('.content')
+const contents = document.querySelectorAll('.content');
 
-content.addEventListener("onscroll", ()=>{
-    content.classList.add('slidein')
-})
+window.addEventListener('scroll', changePlace)
 
+changePlace()
+
+function changePlace(){
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    contents.forEach(content => {
+        const boxTop = content.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            content.classList.add('come')
+        } else {
+            content.classList.remove('come')
+        }
+    })
+}
